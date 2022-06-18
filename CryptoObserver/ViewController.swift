243fa89d2c9ae2f2df8c0dbdc376a16a8359cdf4
DmitryLapata6286 <dmitry.lapata@gmail.com>
2022-06-18 23:09:15 +0300

    feat: add table view

--- CryptoObserver/ViewController.swift
+++ CryptoObserver/ViewController.swift
@@ -7,19 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    private let tabelView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.register(<#T##nib: UINib?##UINib?#>, forCellReuseIdentifier: <#T##String#>)
-        return tableView
-    }
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    // MARK: - Variables
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(CoinTableViewCell.self,
+                           forCellReuseIdentifier: CoinTableViewCell.identifier)
+        return tableView
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Cryptocoins Viewer"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // MARK: - TableView functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CoinTableViewCell.identifier,
+                                                 for: indexPath
+        ) as? CoinTableViewCell else {
+            fatalError()
+        }
+        cell.textLabel?.text = "Hi)"
+        return cell
+    }
 }
 
