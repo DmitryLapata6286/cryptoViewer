1342c6278534d7ebb8e27b59d49ad2a513dde6eb
DmitryLapata6286 <dmitry.lapata@gmail.com>
2022-08-08 20:21:05 +0300

    feat: add image to view

--- CryptoObserver/CoinTableViewCell.swift
+++ CryptoObserver/CoinTableViewCell.swift
@@ -66,28 +66,37 @@ class CoinTableViewCell: UITableViewCell {
         
         let size: CGFloat = contentView.frame.size.height/1.1
         iconView.frame = CGRect(x: 20,
-                                y: 0,
-                                width: size.size.width,
-                                height: size.size.height)
+                                y: (contentView.frame.size.height-size)/2,
+                                width: size,
+                                height: size)
         
         nameLabel.sizeToFit()
         priceLabel.sizeToFit()
         symbolLabel.sizeToFit()
         
-        nameLabel.frame = CGRect(x: 15, y: 0,
+        nameLabel.frame = CGRect(x: 20 + size,
+                                 y: 0,
                                  width: contentView.frame.size.width/2,
                                  height: contentView.frame.size.height/2)
         
-        symbolLabel.frame = CGRect(x: 15,
+        symbolLabel.frame = CGRect(x: 20 + size,
                                    y: contentView.frame.size.height/2,
                                    width: contentView.frame.size.width/2,
                                    height: contentView.frame.size.height/2)
         
         symbolLabel.frame = CGRect(x: contentView.frame.size.width/2,
                                    y: 0,
-                                   width: (contentView.frame.size.width/2)-15,
+                                   width: (contentView.frame.size.width/2)-20,
                                    height: contentView.frame.size.height)
 
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconView.image = nil
+        nameLabel.text = nil
+        priceLabel.text = nil
+        symbolLabel.text = nil
     }
     
     //configure
@@ -96,6 +105,18 @@ class CoinTableViewCell: UITableViewCell {
         nameLabel.text = viewModel.name
         priceLabel.text = viewModel.price
         symbolLabel.text = viewModel.symbol
+        
+        if let url = viewModel.icon {
+            let task = URLSession.shared.dataTask(with: url)
+            {[weak self] data, _, _ in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self?.iconView.image = UIImage(data: data)
+                    }
+                }
+            }
+            task.resume()
+        }
         
     }
 }
